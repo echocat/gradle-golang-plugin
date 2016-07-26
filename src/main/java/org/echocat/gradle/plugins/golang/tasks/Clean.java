6c431c288a2dc8778e134c619258e57217bfaf31
Gregor Noczinski <gregor@noczinski.eu>
2016-07-26 18:33:39 +0200

    * Improve dependency handling

--- src/main/java/org/echocat/gradle/plugins/golang/tasks/Clean.java
+++ src/main/java/org/echocat/gradle/plugins/golang/tasks/Clean.java
@@ -1,17 +1,71 @@
 package org.echocat.gradle.plugins.golang.tasks;
 
-import static org.apache.commons.io.FileUtils.forceDelete;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.HashSet;
+import java.util.Set;
+
+import static java.lang.Boolean.TRUE;
+import static java.nio.file.FileVisitResult.CONTINUE;
+import static java.nio.file.FileVisitResult.SKIP_SUBTREE;
+import static java.nio.file.Files.*;
 
 public class Clean extends GolangTask {
 
     public Clean() {
         setGroup("build");
+        dependsOn(
+            "validate"
+        );
     }
 
     @Override
     public void run() throws Exception {
-        forceDelete(getProject().getBuildDir());
+        final Path path = getProject().getBuildDir().toPath();
+        if (exists(path)) {
+            final Set<Path> doNotCleanSubTreeOfThisPaths = doNotCleanSubTreeOfThisPaths();
+
+
+            walkFileTree(path, new SimpleFileVisitor<Path>() {
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                    delete(file);
+                    return CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                    if (doNotCleanSubTreeOfThisPaths.contains(dir) || isSymbolicLink(dir)) {
+                        delete(dir);
+                        return SKIP_SUBTREE;
+                    }
+                    return CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                    delete(dir);
+                    return CONTINUE;
+                }
+
+            });
+        }
         getDependencyHandler().deleteUnknownDependenciesIfRequired();
+    }
+
+    protected Set<Path> doNotCleanSubTreeOfThisPaths() {
+        final Set<Path> results = new HashSet<>();
+        if (TRUE.equals(getBuild().getUseTemporaryGopath())) {
+            final File packagePath = getGolang().packagePathFor(getBuild().getGopath());
+            final Path packageVendorPath = new File(packagePath, "vendor").toPath();
+            results.add(packageVendorPath);
+        }
+        return results;
     }
 
 }
--- src/main/java/org/echocat/gradle/plugins/golang/tasks/PrepareSources.java
+++ src/main/java/org/echocat/gradle/plugins/golang/tasks/PrepareSources.java
@@ -4,13 +4,19 @@ import org.apache.commons.io.IOUtils;
 import org.codehaus.plexus.util.DirectoryScanner;
 import org.echocat.gradle.plugins.golang.model.BuildSettings;
 import org.echocat.gradle.plugins.golang.model.GolangSettings;
+import org.echocat.gradle.plugins.golang.utils.Executor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 import static java.lang.Boolean.TRUE;
+import static java.nio.file.Files.*;
 import static org.apache.commons.io.FileUtils.forceMkdir;
+import static org.echocat.gradle.plugins.golang.model.OperatingSystem.WINDOWS;
+import static org.echocat.gradle.plugins.golang.model.OperatingSystem.currentOperatingSystem;
 
 public class PrepareSources extends GolangTask {
 
@@ -30,6 +36,8 @@ public class PrepareSources extends GolangTask {
             LOGGER.debug("Prepare GOPATH ({})...", build.getGopath());
             final File projectBasedir = settings.getProjectBasedir();
             final File packagePath = settings.packagePathFor(build.getGopath());
+            final File dependencyCachePath = getDependencies().getDependencyCache();
+            prepareDepencencyCacheIFNeeded(packagePath, dependencyCachePath);
 
             final DirectoryScanner scanner = new DirectoryScanner();
             scanner.setBasedir(projectBasedir);
@@ -40,19 +48,21 @@ public class PrepareSources extends GolangTask {
             for (final String file : scanner.getIncludedFiles()) {
                 final File sourceFile = new File(projectBasedir, file);
                 final File targetFile = new File(packagePath, file);
-                if (!targetFile.exists()
-                    || sourceFile.length() != targetFile.length()
-                    || sourceFile.lastModified() != targetFile.lastModified()) {
-                    atLeastOneCopied = true;
-                    LOGGER.debug("* {}", file);
-                    forceMkdir(targetFile.getParentFile());
-                    try (final InputStream is = new FileInputStream(sourceFile)) {
-                        try (final OutputStream os = new FileOutputStream(targetFile)) {
-                            IOUtils.copy(is, os);
+                if (!sourceFile.equals(dependencyCachePath)) {
+                    if (!targetFile.exists()
+                        || sourceFile.length() != targetFile.length()
+                        || sourceFile.lastModified() != targetFile.lastModified()) {
+                        atLeastOneCopied = true;
+                        LOGGER.debug("* {}", file);
+                        forceMkdir(targetFile.getParentFile());
+                        try (final InputStream is = new FileInputStream(sourceFile)) {
+                            try (final OutputStream os = new FileOutputStream(targetFile)) {
+                                IOUtils.copy(is, os);
+                            }
                         }
+                        //noinspection ResultOfMethodCallIgnored
+                        targetFile.setLastModified(sourceFile.lastModified());
                     }
-                    //noinspection ResultOfMethodCallIgnored
-                    targetFile.setLastModified(sourceFile.lastModified());
                 }
             }
             if (!atLeastOneCopied) {
@@ -60,6 +70,32 @@ public class PrepareSources extends GolangTask {
             }
         } else {
             getState().skipped("SKIPPED");
+        }
+    }
+
+    protected void prepareDepencencyCacheIFNeeded(File packagePath, File dependencyCachePath) throws IOException {
+        final Path packageVendorPath = new File(packagePath, "vendor").toPath();
+
+        if (exists(packageVendorPath)) {
+            if (!isDirectory(packageVendorPath) && !isSymbolicLink(packageVendorPath)) {
+                throw new IllegalStateException(packageVendorPath + " already exists but is not a symbolic link.");
+            }
+        } else {
+            createDirectories(packageVendorPath.getParent());
+            doCreateSymbolicLink(packageVendorPath, dependencyCachePath.toPath());
+        }
+    }
+
+    protected void doCreateSymbolicLink(Path link, Path target) throws IOException {
+        if (currentOperatingSystem() != WINDOWS) {
+            createSymbolicLink(link, target);
+        } else {
+            final String comSpec = System.getenv("ComSpec");
+            Executor.executor()
+                .executable(new File(comSpec))
+                .argument("/C")
+                .arguments("mklink", "/D", "/J", link.toString(), target.toString())
+                .execute();
         }
     }
 
