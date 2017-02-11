e199f235fb3a610ff17ee82af61fd952b7a86dcf
SaSol <mwielgus@outlook.com>
2017-02-12 00:50:15 +0100

    While determining go version GOROOT was unset, when installed in non default location caused go return 2

--- src/main/java/org/echocat/gradle/plugins/golang/model/ToolchainSettings.java
+++ src/main/java/org/echocat/gradle/plugins/golang/model/ToolchainSettings.java
@@ -23,141 +23,142 @@ import static org.echocat.gradle.plugins.golang.utils.Executor.executor;
 
 public class ToolchainSettings {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(ToolchainSettings.class);
-    protected static final Pattern VERSION_RESPONSE_PATTERN = compile("go version ([0-9\\.\\-_a-z]+) .+");
-    @Nonnull
-    private final Project _project;
+	private static final Logger LOGGER = LoggerFactory.getLogger(ToolchainSettings.class);
+	protected static final Pattern VERSION_RESPONSE_PATTERN = compile(
+			"go version ([0-9\\.\\-_a-z]+) .+");
+	@Nonnull
+	private final Project _project;
 
-    private Boolean _forceBuildToolchain;
-    private String _goversion;
-    private Path _goroot;
-    private Boolean _cgoEnabled;
-    private Path _bootstrapGoroot;
-    private URI _downloadUriRoot;
+	private Boolean _forceBuildToolchain;
+	private String _goversion;
+	private Path _goroot;
+	private Boolean _cgoEnabled;
+	private Path _bootstrapGoroot;
+	private URI _downloadUriRoot;
 
-    @Inject
-    public ToolchainSettings(boolean initialize, @Nonnull Project project) {
-        _project = project;
-        if (initialize) {
-            _goversion = DEFAULT_GO_VERSION;
-            _downloadUriRoot = DEFAULT_DOWNLOAD_URI_ROOT;
-        }
-    }
+	@Inject
+	public ToolchainSettings(boolean initialize, @Nonnull Project project) {
+		_project = project;
+		if (initialize) {
+			_goversion = DEFAULT_GO_VERSION;
+			_downloadUriRoot = DEFAULT_DOWNLOAD_URI_ROOT;
+		}
+	}
 
-    public Boolean getForceBuildToolchain() {
-        return _forceBuildToolchain;
-    }
+	public Boolean getForceBuildToolchain() {
+		return _forceBuildToolchain;
+	}
 
-    public void setForceBuildToolchain(Boolean forceBuildToolchain) {
-        _forceBuildToolchain = forceBuildToolchain;
-    }
+	public void setForceBuildToolchain(Boolean forceBuildToolchain) {
+		_forceBuildToolchain = forceBuildToolchain;
+	}
 
-    public String getGoversion() {
-        return _goversion;
-    }
+	public String getGoversion() {
+		return _goversion;
+	}
 
-    public void setGoversion(String goversion) {
-        _goversion = goversion;
-    }
+	public void setGoversion(String goversion) {
+		_goversion = goversion;
+	}
 
-    public Path getGoroot() {
-        return _goroot;
-    }
+	public Path getGoroot() {
+		return _goroot;
+	}
 
-    public void setGoroot(Path goroot) {
-        _goroot = goroot;
-    }
+	public void setGoroot(Path goroot) {
+		_goroot = goroot;
+	}
 
-    public Boolean getCgoEnabled() {
-        return _cgoEnabled;
-    }
+	public Boolean getCgoEnabled() {
+		return _cgoEnabled;
+	}
 
-    public void setCgoEnabled(Boolean cgoEnabled) {
-        _cgoEnabled = cgoEnabled;
-    }
+	public void setCgoEnabled(Boolean cgoEnabled) {
+		_cgoEnabled = cgoEnabled;
+	}
 
-    public Path getBootstrapGoroot() {
-        return _bootstrapGoroot;
-    }
+	public Path getBootstrapGoroot() {
+		return _bootstrapGoroot;
+	}
 
-    public void setBootstrapGoroot(Path bootstrapGoroot) {
-        _bootstrapGoroot = bootstrapGoroot;
-    }
+	public void setBootstrapGoroot(Path bootstrapGoroot) {
+		_bootstrapGoroot = bootstrapGoroot;
+	}
 
-    public URI getDownloadUriRoot() {
-        return _downloadUriRoot;
-    }
+	public URI getDownloadUriRoot() {
+		return _downloadUriRoot;
+	}
 
-    public void setDownloadUriRoot(URI downloadUriRoot) {
-        _downloadUriRoot = downloadUriRoot;
-    }
+	public void setDownloadUriRoot(URI downloadUriRoot) {
+		_downloadUriRoot = downloadUriRoot;
+	}
 
-    @Nullable
-    public String goBinaryVersionOf(Path goroot) {
-        final Path goBinary = goBinaryOf(goroot);
-        if (!isExecutable(goBinary)) {
-            return null;
-        }
-        final String stdout;
-        try {
-            stdout = executor(goBinary)
-                .argument("version")
-                .execute()
-                .getStdoutAsString()
-            ;
-        } catch (final IOException e) {
-            LOGGER.debug(e.getMessage(), e);
-            return null;
-        }
+	@Nullable
+	public String goBinaryVersionOf(Path goroot) {
+		final Path goBinary = goBinaryOf(goroot);
+		if (!isExecutable(goBinary)) {
+			return null;
+		}
+		final String stdout;
+		try {
+			stdout = executor(goBinary)
+					.env("GOROOT", goBinary.getParent().toAbsolutePath().toString())
+					.argument("version").execute().getStdoutAsString();
+		}
+		catch (final IOException e) {
+			LOGGER.debug(e.getMessage(), e);
+			return null;
+		}
 
-        final Matcher matcher = VERSION_RESPONSE_PATTERN.matcher(stdout.trim());
-        if (!matcher.matches()) {
-            throw new IllegalStateException(goBinary + " does not respond as expected. Got: " + stdout);
-        }
-        return matcher.group(1);
-    }
+		final Matcher matcher = VERSION_RESPONSE_PATTERN.matcher(stdout.trim());
+		if (!matcher.matches()) {
+			throw new IllegalStateException(
+					goBinary + " does not respond as expected. Got: " + stdout);
+		}
+		return matcher.group(1);
+	}
 
-    @Nonnull
-    public Path goBinaryOf(Path goroot) {
-        if (goroot == null) {
-            throw new IllegalArgumentException("There was no goroot provided.");
-        }
-        return toolchainBinaryOf("go", goroot);
-    }
+	@Nonnull
+	public Path goBinaryOf(Path goroot) {
+		if (goroot == null) {
+			throw new IllegalArgumentException("There was no goroot provided.");
+		}
+		return toolchainBinaryOf("go", goroot);
+	}
 
-    @Nonnull
-    public Path getGoBinary() {
-        return toolchainBinary("go");
-    }
+	@Nonnull
+	public Path getGoBinary() {
+		return toolchainBinary("go");
+	}
 
-    @Nonnull
-    public Path toolchainBinaryOf(String name, Path goroot) {
-        if (goroot == null) {
-            throw new IllegalArgumentException("There was no goroot provided.");
-        }
-        return goroot.resolve("bin").resolve(name + getExecutableSuffix());
-    }
+	@Nonnull
+	public Path toolchainBinaryOf(String name, Path goroot) {
+		if (goroot == null) {
+			throw new IllegalArgumentException("There was no goroot provided.");
+		}
+		return goroot.resolve("bin").resolve(name + getExecutableSuffix());
+	}
 
-    @Nonnull
-    public Path toolchainBinary(String name) {
-        return toolchainBinaryOf(name, getGoroot());
-    }
+	@Nonnull
+	public Path toolchainBinary(String name) {
+		return toolchainBinaryOf(name, getGoroot());
+	}
 
-    @Nonnull
-    public Path getGorootSourceRoot() {
-        final Path goroot = getGoroot();
-        if (goroot == null) {
-            throw new IllegalStateException("There is no goroot cofigured.");
-        }
-        return goroot.resolve("src");
-    }
+	@Nonnull
+	public Path getGorootSourceRoot() {
+		final Path goroot = getGoroot();
+		if (goroot == null) {
+			throw new IllegalStateException("There is no goroot cofigured.");
+		}
+		return goroot.resolve("src");
+	}
 
-    @Nonnull
-    public String getExecutableSuffix() {
-        if (currentOperatingSystem() == WINDOWS) {
-            return ".exe";
-        }
-        return "";
-    }
+	@Nonnull
+	public String getExecutableSuffix() {
+		if (currentOperatingSystem() == WINDOWS) {
+			return ".exe";
+		}
+		return "";
+	}
 
 }
